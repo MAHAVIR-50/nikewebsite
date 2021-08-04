efcdf1925e1ae72b543224f25c111c653e1d7186
Mahavir <mahavir240@gmail.com>
2021-08-04 16:54:10 +0530

    Add code

--- amazonwebsiteTest/src/test/java/AmazonWebTestCases/FashionTest.java
+++ amazonwebsiteTest/src/test/java/AmazonWebTestCases/FashionTest.java
@@ -1,6 +1,37 @@
 package AmazonWebTestCases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import AmazonPages.AmazonHomePage;
+
 public class FashionTest {
+WebDriver driver;
 	
+	@BeforeMethod
+	public void setUp() {
+		
+		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/chromedriver.exe");
+		driver= new ChromeDriver();
+		AmazonHomePage.openBrowser(driver);
+		driver.manage().window().maximize();
+		
+	}
+	@AfterMethod
+	public void tearDown() throws InterruptedException {
+		
+		Thread.sleep(5000);
+		driver.quit();
+	}
+	@Test
+	public void fashionLink() {
+		
+		driver.findElement(By.linkText("Fashion")).click();
+	}
 
 }
