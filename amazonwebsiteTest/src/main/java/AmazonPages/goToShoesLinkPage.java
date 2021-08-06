b46ca09ae0783185a8138b52c08b334df3b2ea06
Mahavir <mahavir240@gmail.com>
2021-08-06 15:20:53 +0530

    Add new test

--- amazonwebsiteTest/src/main/java/AmazonPages/goToShoesLinkPage.java
+++ amazonwebsiteTest/src/main/java/AmazonPages/goToShoesLinkPage.java
@@ -1,7 +1,9 @@
 package AmazonPages;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
 
 public class goToShoesLinkPage {
 	public static String clickOnMenLink = "Men";
@@ -13,10 +15,21 @@ public class goToShoesLinkPage {
 
 	public static String clickOnAddCartButton= "//input[@value='Add to Cart']";
 	public static String clickOnGoToCartButton = "Cart";
+	public static String validatePrice = "//span[@class='a-size-medium a-color-base sc-price sc-white-space-nowrap sc-product-price a-text-bold']";
 	//public static String clickOnDeleteLink = "//input[@aria-label='Delete Adidas Men&#39;s ULTIMASHOW Running Shoe,RED, 7 UK']";
 	
-	
+	public static void validatePriceOfShoes(String validationPrice,WebDriver driver) throws InterruptedException {
+		Thread.sleep(5000);
+		String actualPrice = driver.findElement(By.xpath(goToShoesLinkPage.validatePrice)).getText();
+		if(actualPrice.equals(validationPrice)) {
+			
+			Assert.fail("Actual Price and Validation Price did not match. Actual Price : " +actualPrice + " Validation Price :" +validationPrice );
+		}else 
+			Reporter.log("Actual Price and Validation Price match. Actual Price : " +actualPrice+ "Validation Price :" +validationPrice);
+			
 		
-	
+	}
 	
 }
+
+	
---
+++ amazonwebsiteTest/src/test/java/AmazonWebTestCases/SearchShoesTest.java
@@ -0,0 +1,58 @@
+package AmazonWebTestCases;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+import AmazonCommon.AmazonCommonCode;
+import AmazonPages.AddressPage;
+import AmazonPages.AmazonHomePage;
+import AmazonPages.goToClothingPage;
+import AmazonPages.SignInPage;
+import AmazonPages.goToShoesLinkPage;
+
+public class SearchShoesTest {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+		
+		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/chromedriver.exe");
+		driver= new ChromeDriver();
+		AmazonHomePage.openBrowser(driver);
+		driver.manage().window().maximize();
+		
+	}
+	@AfterMethod
+	public void tearDown() throws InterruptedException {
+		
+		Thread.sleep(5000);
+		driver.quit();
+	}
+	
+	
+	@Test
+	public void validateShoesPrice() throws InterruptedException {
+		
+		AmazonHomePage.fashionLink(driver);
+		AmazonHomePage.specialClickOnMenLink(driver);
+		AmazonHomePage.selectProductsLinks(driver);
+		AmazonCommonCode.handleWindowTab(driver);
+		AmazonHomePage.SelectShoesSizeByDrop(driver);
+		AmazonHomePage.productAddToCart(driver);
+		goToShoesLinkPage.validatePriceOfShoes("2,795.00",driver);				
+		
+	}
+		
+
+}
+
+
